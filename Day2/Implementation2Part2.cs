503d9171f73b2c9a52875fe7dfd465f0c2e5f993
karl bezzina <karlbezzina@gmail.com>
2023-12-14 10:56:35 +0100

    Completed day 2 part 2

---
+++ Day2/Implementation2Part2.cs
@@ -0,0 +1,62 @@
+﻿namespace Day2
+{
+    public class Implementation2Part2
+    {
+        public string ReadInputFile(string filePath)
+        {
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        public long SolveProblem(string problem)
+        {
+            string[] lines = problem.Split("\n");
+            long sumOfPowerCubes = 0;
+            object lockObject = new object();
+            foreach (string line in lines)
+            {
+                if (line != "")
+                {
+                    string[] gameDescription = line.Split(": ");
+                    long powerOfCubes = CalculatePowerOfCubes(gameDescription[1]);
+                    sumOfPowerCubes += powerOfCubes;
+                }
+            }
+
+            return sumOfPowerCubes;
+        }
+        public long CalculatePowerOfCubes(string game)
+        {
+            long red = 0;
+            long green = 0;
+            long blue = 0;
+
+            string[] subsets = game.Split("; ");
+            foreach (string subset in subsets)
+            {
+                string[] values = subset.Split(", ");
+                foreach (string value in values)
+                {
+                    string[] cubeColorValue = value.Split(" ");
+                    long parsedValue = long.Parse(cubeColorValue[0]);
+                    switch (cubeColorValue[1])
+                    {
+                        case "red":
+                            red = red > parsedValue ? red : parsedValue;
+                            break;
+                        case "green":
+                            green = green > parsedValue ? green : parsedValue;
+                            break;
+                        case "blue":
+                            blue = blue > parsedValue ? blue : parsedValue;
+                            break;
+                    }
+                }
+            }
+
+            return red * green * blue;
+        }
+    }
+}
--- Day2/Program.cs
+++ Day2/Program.cs
@@ -6,3 +6,9 @@ string problem = implementation.ReadInputFile(Environment.CurrentDirectory + "\\
 Console.WriteLine("Input file read");
 long sumOfIDs = implementation.SolveProblem(problem);
 Console.WriteLine("\nSum of IDs for Input: " + sumOfIDs);
+
+Implementation2Part2 implementation2Part2 = new Implementation2Part2();
+string part2Problem = implementation2Part2.ReadInputFile(Environment.CurrentDirectory + "\\input.txt");
+Console.WriteLine("Input file read");
+long sumOfCubes = implementation2Part2.SolveProblem(part2Problem);
+Console.WriteLine("\nSum of Cubes for Input: " + sumOfCubes);
